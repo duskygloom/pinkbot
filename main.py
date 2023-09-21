f99bf67a2bdd88ede543d19b52604662166f95be
duskygloom <sarkarharsh708@gmail.com>
2023-09-21 07:50:12 +0530

    added environment variable support

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-import secret, logging, asyncio, bot
+import logging, asyncio, bot, sys
+
+try:
+    from secret import bot_token
+except ModuleNotFoundError:
+    import os
+    bot_token = os.getenv("DISCORD_TOKEN")
+    if bot_token is None:
+        logging.error("No token found.")
+        sys.exit(0)
 
 if __name__ == "__main__":
     try:
         bot_instance = asyncio.run(bot.create_bot())
-        bot_instance.run(secret.bot_token)
+        bot_instance.run(bot_token)
     except Exception as e:
         logging.error(f"Unhandled error: {e}")
