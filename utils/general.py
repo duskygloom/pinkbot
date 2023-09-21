287c18bdff65355f2c452a4b92a3a0d41c3c7153
duskygloom <sarkarharsh708@gmail.com>
2023-09-21 07:56:31 +0530

    tenor token from environment variable

--- utils/general.py
+++ utils/general.py
@@ -1,4 +1,9 @@
-import discord, json, secret, requests, random
+import discord, json, requests, random, logging, os
+
+try:
+    from secret import tenor_api_key
+except ModuleNotFoundError:
+    tenor_api_key = os.getenv("TENOR_KEY")
 
 def get_member_name(member: discord.Member) -> str:
     name = member.nick
@@ -6,10 +11,11 @@ def get_member_name(member: discord.Member) -> str:
         name = member.display_name
     return name
 
-def get_gif_url(query: str, client_key: str) -> str:
-    apikey = secret.tenor_api_key
+def get_gif_url(query: str, client_key: str) -> str | None:
+    if tenor_api_key is None:
+        return "" 
     limit = 8
-    url = f"https://tenor.googleapis.com/v2/search?q='{query}'&key={apikey}&client_key={client_key}&limit={limit}"
+    url = f"https://tenor.googleapis.com/v2/search?q='{query}'&key={tenor_api_key}&client_key={client_key}&limit={limit}"
     r = requests.get(url)
     if r.status_code == 200:
         top8_gifs = json.loads(r.content)
